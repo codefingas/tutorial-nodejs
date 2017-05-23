03ea534694ce87f008cc490a10e9e167e210c0c4
fabien kerinec <fabien.kerinec@gmail.com>
2017-05-23 15:19:01 +0200

    step 22
    currently step 22 have un prob with img when select a tag

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next){
+		const isPhoto = file.mimetype.startsWith('image/');
+		if(isPhoto){
+			next(null, true);
+		}else({message : 'that filetype isn\'t allowed'}, false);
+	}
+};
 
 exports.homePage = (req, res) =>{
 	console.log(req.name);
@@ -9,6 +23,24 @@ exports.homePage = (req, res) =>{
 exports.addStore =(req, res) =>{
 	res.render('editStore', {title:'Add Store'});
 };
+
+exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+	/*check if there is no new file to resize*/
+	if(!req.file){
+		next(); /*skip to the next middleware*/
+		return;
+	}
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}` ;
+	/*now resize*/
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+	/*one we have writter photo to our file system*/
+	next();
+};
+
 
 exports.createStore = async (req, res) => {
 	const store = await (new Store(req.body)).save();
@@ -43,4 +75,22 @@ exports.updateStore = async(req, res) =>{
 	req.flash('success', `successfully updated ${store.name}. <a href="/stores/${store.slug}"> view store -></a>`)
 	/*Redirect then the store and tell its ok*/
 	res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res) => {
+	const store = await Store.findOne({slug : req.params.slug});
+	if(!store) return next();
+	res.render('store', {store:store, title:store.name})
+};
+
+exports.getStoresByTag = async (req, res) => {
+  	const tag = req.params.tag;
+  	const tagQuery = tag || { $exists: true };
+
+  	const tagsPromise = Store.getTagsList();
+  	const storesPromise = Store.find({ tags: tagQuery });
+  	const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+
+  	res.render('tag', { tags, title: 'Tags', tag, stores });
 };
--- models/Store.js
+++ models/Store.js
@@ -2,7 +2,6 @@ const mongoose =  require('mongoose');
 mongoose.Promise = global.Promise;
 const slug =require('slugs');
 
-
 const storeSchema = new mongoose.Schema({
 	name : {
 		type : String,
@@ -33,17 +32,34 @@ const storeSchema = new mongoose.Schema({
 			type : String,
 			required : 'you must supply an adress'
 		}
-	}
+	},
+	photo:String
 });
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
 	if(!this.isModified('name')){
 		next(); //skip it
 		return; //stop this function from running
 	}
 	this.slug = slug(this.name);
+	/*find other store that have a slug be the same*/
+	const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+	const storesWithSlug = await this.constructor.find({slug : slugRegEx});
+
+	if(storesWithSlug.length){
+		this.slug = `${this.slug}-${storesWithSlug.length +1}`;
+	}
 	next();
-	//todo  make more resilient so slugs are : unique
+	//todo  make more resilient so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function(){
+	/*Class by / Group by / Sort by inverse*/
+	return this.aggregate([
+		{$unwind : '$tags'},
+		{$group: { _id : '$tags', count : {$sum:1}}},
+		{$sort : {count:-1}}
+	]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
